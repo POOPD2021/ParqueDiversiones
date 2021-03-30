0dd4e80f19a34d5e4375e2ef10c6f394e30f102b
Andresjales <andres.alvareze@upb.edu.co>
2021-03-30 17:23:33 -0500

    Creé las herencias de la clase atracción

---
+++ Virtual.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParqueDiversiones
+{
+    class Virtual : Atraccion
+    {
+        public Virtual() : base()
+        {
+        }
+    }
+}
