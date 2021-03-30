ea15af9bf39f73fed3eff759d755ab3e3b14aa9b
Samuel Angel Palacio Mesa <samuel.palacio@upb.edu.co>
2021-03-30 16:31:00 -0500

    Agregar archivos de proyecto.

---
+++ Sistema.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ParqueDiversiones
+{
+    class Sistema
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
