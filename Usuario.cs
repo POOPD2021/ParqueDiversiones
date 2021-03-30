e871224d2433be186a029092440b363fe1816aa4
Samuel Angel Palacio Mesa <samuel.palacio@upb.edu.co>
2021-03-30 17:29:57 -0500

    Creé usuario

---
+++ Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParqueDiversiones
+{
+    class Usuario : Persona
+    {
+        private Atraccion encargado;
+        public Usuario (string nombre, DateTime fechaNacimiento, long docID, Atraccion atraccion): base (nombre,fechaNacimiento,docID)
+        {
+            this.encargado = atraccion;
+        }
+        public void AsignarAtraccion(Atraccion atraccion)
+        {
+
+        }
+    }
+}
