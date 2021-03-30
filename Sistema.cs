00d17da8451025028beb2e939ef631875fa5b30c
Stefania Espinal Ramirez <stefania.espinal@upb.edu.co>
2021-03-30 17:31:15 -0500

    Metodos de Sistema Stefa

--- Sistema.cs
+++ Sistema.cs
@@ -9,8 +9,43 @@ namespace ParqueDiversiones
         {
             Console.WriteLine("Bienvenidos al Sistema");
             List<Persona> personas = new List<Persona>();
-            List<Persona> atracciones = new List<Persona>();
+            List<Atraccion> atracciones = new List<Atraccion>();
             List<ReporteCostos> reporteCostos = new List<ReporteCostos>();
+
+            static void GenerarReporte()
+            {
+
+            }
+
+            static void ResgistrarEmpleado(string nombre, long docID, DateTime fechaNacimiento, int edad)
+            {
+
+            }
+
+            static void GenerarManilla(double saldo)
+            {
+
+            }
+
+            static void RegistrarUsuario(string nombre, long docID, DateTime fechaNacimiento, int edad, double estatura, Manilla dueño)
+            {
+
+            }
+
+            static void RegistrarAtraccion(string codigo, string nombre, int limite_de_edad, double limite_de_estatura, double costo, string descripcion, bool operando)
+            {
+
+            }
+
+            static void ConsutarInfoAtraccion(Atraccion atraccion)
+            {
+
+            }
+
+            static void ConsultarInfoPersona(Persona persona)
+            {
+
+            }
         }
     }
 }
