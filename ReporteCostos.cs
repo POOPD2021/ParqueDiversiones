3ec788bdefbc16eaf7637ee601dfb61cfa415a4c
Stefania Espinal Ramirez <stefania.espinal@upb.edu.co>
2021-03-30 17:12:03 -0500

    Agregue atributos y propiedades en Reporte Stefania

--- ReporteCostos.cs
+++ ReporteCostos.cs
@@ -8,5 +8,16 @@ namespace ParqueDiversiones
 {
     class ReporteCostos
     {
+        private double nroIngresosDia;
+        private double ingresosDiaAtraccion;
+        private double costosDiaAtraccion;
+        private double costosDiaTotales;
+        private DateTime fecha;
+
+        public double NroIngresosDia { get => nroIngresosDia;}
+        public double IngresosDiaAtraccion { get => ingresosDiaAtraccion;}
+        public double CostosDiaAtraccion { get => costosDiaAtraccion;}
+        public double CostosDiaTotales { get => costosDiaTotales;}
+        public DateTime Fecha { get => fecha;}
     }
 }
